fc50cdb85273643dc2d3e0957cf4586a295129e8
ipknHama <ipknhama@gmail.com>
2014-01-24 10:37:32 +0900

    Clean up test code

--- test.cpp
+++ test.cpp
@@ -48,6 +48,70 @@ void fail(string msg)
 #define IGNORED_TEST(x) \
     void test_##x##_()
 
+TEST(simple_example)
+{
+    // example data structure
+
+    struct student
+    {
+        dwstring name;
+        int score;
+        student(){}
+        student(const wstring& name, int score) : name(name), score(score) {}
+    };
+
+    struct classroom
+    {
+        dstring class_name;
+        dvector<student> students;
+    };
+
+    // building sample data
+
+    vector<student> testData;
+    testData.push_back(student(L"Alice",2));
+    testData.push_back(student(L"Bob",5));
+    testData.push_back(student(L"\ud55c\uae00",13));
+    classroom c;
+    c.class_name = "1001";
+    c.students = testData;
+    {
+        dvector<student> move_test(testData);
+        c.students = std::move(move_test);
+    }
+
+    // Dump to stringstream
+    // NOTE: No need to write serialization code for struct student and struct classroom.
+
+    ostringstream os;
+    dumpable::write(c, os);
+
+    // Destroy the contents of sample data.
+
+    c.students.clear();
+    c.class_name = "invalid";
+
+    // read from dumped data
+    // NOTE: from_dumped_buffer is just a constant time operation! 
+    //   (see implementation in dumpable.h)
+    // You can even use a pointer from mmap.
+
+    string buffer = os.str();
+    const classroom* c2 = dumpable::from_dumped_buffer<classroom>(buffer.data());
+
+    // Check contents of c2
+    // You should keep the buffer given to from_dumped_buffer function.
+    // These data are only valid when the buffer is valid.
+
+    ASSERT_EQUAL("1001", c2->class_name);
+    ASSERT_EQUAL(L"Alice", c2->students[0].name);
+    ASSERT_EQUAL(2, c2->students[0].score);
+    ASSERT_EQUAL(L"Bob", c2->students[1].name);
+    ASSERT_EQUAL(5, c2->students[1].score);
+    ASSERT_EQUAL(L"\ud55c\uae00", c2->students[2].name);
+    ASSERT_EQUAL(13, c2->students[2].score);
+}
+
 TEST(linked_list)
 {
     struct node
@@ -69,7 +133,7 @@ TEST(linked_list)
     // dptr<node> can be used as node*
 
     int sum = 0;
-    node* p = &n1;
+    const node* p = &n1;
     ASSERT_EQUAL(1, p->d);
     ASSERT_EQUAL(&n2, p->n);
     while(p)
@@ -85,13 +149,14 @@ TEST(linked_list)
     dumpable::write(n1, os);
 
     // destroy original data
+
     n3.d = n2.d = n1.d = -100;
     n3.n = n2.n = n1.n = nullptr;
 
     // read from dumped data
 
     string buffer = os.str();
-    p = dumpable::from_dumped_buffer<node>(&buffer[0]);
+    p = dumpable::from_dumped_buffer<node>(buffer.data());
 
     // recheck data
 
@@ -220,11 +285,11 @@ TEST(map)
 
 
     string buffer = os.str();
-    decltype(data)* p = dumpable::from_dumped_buffer<decltype(data)>(&buffer[0]);
+    const decltype(data)* p = dumpable::from_dumped_buffer<const decltype(data)>(buffer.data());
 
     
     {
-        dmap<int, dstring>& data = *p;
+        const dmap<int, dstring>& data = *p;
         ASSERT_EQUAL("one", data.find(1)->second);
         ASSERT_EQUAL("two", data.find(2)->second);
         ASSERT_EQUAL("five", data.find(5)->second);
@@ -236,68 +301,16 @@ TEST(map)
 
 }
 
-TEST(vector_and_string)
-{
-    struct empty_struct_for_test {};
-    struct student
-    {
-        empty_struct_for_test x1;
-        dwstring name;
-        empty_struct_for_test x2;
-        int score;
-        empty_struct_for_test x3;
-        student(){}
-        student(const wstring& name, int score) : name(name), score(score) {}
-    };
-    struct classroom
-    {
-        dstring class_name;
-        dvector<student> students;
-    };
-
-    vector<student> testData;
-    testData.push_back(student(L"Alice",2));
-    testData.push_back(student(L"Bob",5));
-    testData.push_back(student(L"\ud55c\uae00",13));
-    classroom c;
-    c.class_name = "1001";
-    c.students = testData;
-    {
-        dvector<student> move_test(testData);
-        c.students = std::move(move_test);
-    }
-
-    ostringstream os;
-    dumpable::write(c, os);
-
-    c.students.clear();
-
-    string buffer = os.str();
-    classroom* c2 = dumpable::from_dumped_buffer<classroom>(&buffer[0]);
-
-    ASSERT_EQUAL("1001", c2->class_name);
-    ASSERT_EQUAL(L"Alice", c2->students[0].name);
-    ASSERT_EQUAL(2, c2->students[0].score);
-    ASSERT_EQUAL(L"Bob", c2->students[1].name);
-    ASSERT_EQUAL(5, c2->students[1].score);
-    ASSERT_EQUAL(L"\ud55c\uae00", c2->students[2].name);
-    ASSERT_EQUAL(13, c2->students[2].score);
-
-    classroom x = std::move(*c2);
-    ASSERT_EQUAL("1001", x.class_name);
-    ASSERT_EQUAL(L"Alice", x.students[0].name);
-    ASSERT_EQUAL(2, x.students[0].score);
-    ASSERT_EQUAL(L"Bob", x.students[1].name);
-    ASSERT_EQUAL(5, x.students[1].score);
-    ASSERT_EQUAL(L"\ud55c\uae00", x.students[2].name);
-    ASSERT_EQUAL(13, x.students[2].score);
-}
-
 TEST(basic_implementation)
 {
+    struct embedded_empty
+    {
+        // intentionally empty
+    };
     struct embedded 
     {
         int x;
+        dptr<embedded_empty> y;
     };
     struct data
     {
@@ -309,6 +322,7 @@ TEST(basic_implementation)
     d.a = 1;
     d.b = new embedded;
     d.b->x = 2;
+    d.b->y = new embedded_empty;
     d.c = 3;
 
     ostringstream os;
